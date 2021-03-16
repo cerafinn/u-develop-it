dbf2d1e5f438cbfdfbc74554320bf11e25edfe7c
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-15 22:35:23 -0400

    working on joining tables

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id`;
   const params = []
 
   db.all(sql, params, (err, rows) => {
@@ -32,7 +36,12 @@ app.get('/api/candidates', (req, res) => {
 });
 
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`
+  const sql = `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id
+  WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.get(sql, params, (err, row) => {
